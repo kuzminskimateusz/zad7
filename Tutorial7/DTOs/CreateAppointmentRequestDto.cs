5d33e620f1eed4f7737be79043c826eb786bb9ef
s34201@pjwstk.edu.pl <mati4801@gmail.com>
2026-04-22 11:17:03 +0200

    create project

---
+++ Tutorial7/DTOs/CreateAppointmentRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tutorial7.DTOs;
+
+public class CreateAppointmentRequestDto
+{
+    public int IdAppointment { get; set; }
+    public int IdDoctor { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Status { get; set; } = "Scheduled";
+    public string Reason { get; set; } = string.Empty;
+}
