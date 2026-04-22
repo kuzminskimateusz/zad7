a4c557b020d3c187842f844e0276b34b885db2c1
s34201@pjwstk.edu.pl <mati4801@gmail.com>
2026-04-22 11:20:38 +0200

    AppointmentDetailsDto

--- Tutorial7/DTOs/AppointmentDetailsDto.cs
+++ Tutorial7/DTOs/AppointmentDetailsDto.cs
@@ -2,5 +2,13 @@
 
 public class AppointmentDetailsDto
 {
-    
+    public int IdAppointment { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public string Reason { get; set; } = string.Empty;
+    public string PatientFullName { get; set; } = string.Empty;
+    public string PatientEmail { get; set; } = string.Empty;
 }
